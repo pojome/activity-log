b4b64ce62e039d5d952c7a0acc330173efa17b78
Yakir Sitbon <kingyes1@gmail.com>
2013-04-02 16:40:57 +0300

    Deleted unused lines

--- classes/admin-ui.php
+++ classes/admin-ui.php
@@ -5,58 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 class HT_Admin_Ui {
 	
 	public function admin_init() {
-		wp_enqueue_style( 'history-timeline', plugins_url( '/admin-ui/', HISTORY_TIMELINE_BASE ) . 'history-timeline.css' );
-	}
-
-	/**
-	 * @param array    $args
-	 * @param HT_Model $history
-	 * 
-	 * @deprecated
-	 */
-	public function viewPartialHistory( $args = array(), HT_Model $history ) {
-		$rows = $history->getLastResult( $args );
-
-		if ( ! $rows ) {
-			echo '<p>No have any logs.</p>';
-
-			return;
-		}
-
-		foreach ( $rows as $row ) {
-			$user       = false;
-			$userText   = 'by ';
-			$objectName = $row->object_name;
-
-			if ( ! empty( $row->user_id ) ) {
-				$user = get_user_by( 'id', $row->user_id );
-			}
-
-			if ( $user ) {
-				$userText .= '<a href="user-edit.php?user_id=' . $user->ID . '">' . $user->user_login . '</a>';
-			}
-			else {
-				$userText .= 'Guest';
-			}
-
-			$userText .= ' (' . $row->histIP . ')';
-
-			if ( $row->object_type === 'Post' ) {
-				$objectName = '<a href="post.php?post=' . $row->object_id . '&action=edit">' . $row->object_name . '</a>';
-			}
-
-
-			?>
-			<div id="history-system-item-<?php echo $row->histid; ?>" class="row">
-				<div class="type"><?php echo $row->object_type; ?><?php echo ( ! empty( $row->object_subtype ) ) ? ' (' . $row->object_subtype . ')' : ''; ?></div>
-				<div class="name"><?php echo $objectName; ?></div>
-				<div class="action"> was <?php echo $row->action; ?></div>
-				<div class="dateaction"><?php echo human_time_diff( $row->histTime, current_time( 'timestamp' ) ); ?></div>
-				<div class="dateshow"><?php echo date( 'd/m/Y H:i', $row->histTime ); ?></div>
-				<div class="user"><?php echo $userText; ?></div>
-			</div>
-		<?php
-		}
+		//wp_enqueue_style( 'history-timeline', plugins_url( '/admin-ui/', HISTORY_TIMELINE_BASE ) . 'history-timeline.css' );
 	}
 
 	public function create_admin_menu() {
@@ -75,7 +24,6 @@ class HT_Admin_Ui {
 				<input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
 				<?php $history_table->display(); ?>
 			</form>
-			
 		</div>
 
 	<?php
