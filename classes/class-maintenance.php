7215a06724fe742f7b38ac3de5b30fab42be620c
Maor <maorhaz@gmail.com>
2013-04-03 00:24:03 +0300

    Removed check beforfe creating the table

--- classes/class-maintenance.php
+++ classes/class-maintenance.php
@@ -8,26 +8,22 @@ class HT_Maintenance {
 		/** @var $wpdb wpdb */
 		global $wpdb;
 
-		if ( $wpdb->get_var( "SHOW TABLES LIKE '$wpdb->history_timeline'" ) != $wpdb->history_timeline ) :
+		$sql = "CREATE TABLE IF NOT EXISTS `{$wpdb->history_timeline}` (
+					  `histid` int(11) NOT NULL AUTO_INCREMENT,
+					  `userCaps` varchar(70) NOT NULL DEFAULT 'guest',
+					  `action` varchar(255) NOT NULL,
+					  `object_type` varchar(255) NOT NULL,
+					  `object_subtype` varchar(255) NOT NULL DEFAULT '',
+					  `object_name` varchar(255) NOT NULL,
+					  `object_id` int(11) NOT NULL DEFAULT '0',
+					  `user_id` int(11) NOT NULL DEFAULT '0',
+					  `histIP` varchar(55) NOT NULL DEFAULT '127.0.0.1',
+					  `histTime` int(11) NOT NULL DEFAULT '0',
+					  PRIMARY KEY (`histid`)
+				) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;";
 
-			$sql = "CREATE TABLE IF NOT EXISTS `{$wpdb->history_timeline}` (
-						  `histid` int(11) NOT NULL AUTO_INCREMENT,
-						  `userCaps` varchar(70) NOT NULL DEFAULT 'guest',
-						  `action` varchar(255) NOT NULL,
-						  `object_type` varchar(255) NOT NULL,
-						  `object_subtype` varchar(255) NOT NULL DEFAULT '',
-						  `object_name` varchar(255) NOT NULL,
-						  `object_id` int(11) NOT NULL DEFAULT '0',
-						  `user_id` int(11) NOT NULL DEFAULT '0',
-						  `histIP` varchar(55) NOT NULL DEFAULT '127.0.0.1',
-						  `histTime` int(11) NOT NULL DEFAULT '0',
-						  PRIMARY KEY (`histid`)
-					) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;";
-
-			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-			dbDelta( $sql );
-
-		endif;
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
 		
 		update_option( 'history_timeline_db_version', '0.1' );
 	}
