4399a91594c98f35eac9aed7f0c4f0fdd836aa4a
Maor <maorhaz@gmail.com>
2013-04-03 00:12:49 +0300

    Added a check if DB exists before attemtping to create it, added a uninstall hook, added db version control in case we ever need to upgrade the table schema in the future

--- classes/class-maintenance.php
+++ classes/class-maintenance.php
@@ -4,27 +4,43 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class HT_Maintenance {
 	
-	public static function activated() {
+	public static function activate() {
 		/** @var $wpdb wpdb */
 		global $wpdb;
 
-		$sql = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}history_timeline` (
-					  `histid` int(11) NOT NULL AUTO_INCREMENT,
-					  `userCaps` varchar(70) NOT NULL DEFAULT 'guest',
-					  `action` varchar(255) NOT NULL,
-					  `object_type` varchar(255) NOT NULL,
-					  `object_subtype` varchar(255) NOT NULL DEFAULT '',
-					  `object_name` varchar(255) NOT NULL,
-					  `object_id` int(11) NOT NULL DEFAULT '0',
-					  `user_id` int(11) NOT NULL DEFAULT '0',
-					  `histIP` varchar(55) NOT NULL DEFAULT '127.0.0.1',
-					  `histTime` int(11) NOT NULL DEFAULT '0',
-					  PRIMARY KEY (`histid`)
-				) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;";
+		if ( $wpdb->get_var( "SHOW TABLES LIKE '$wpdb->history_timeline'" ) != $wpdb->history_timeline ) :
 
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-		dbDelta( $sql );
+			$sql = "CREATE TABLE IF NOT EXISTS `{$wpdb->history_timeline}` (
+						  `histid` int(11) NOT NULL AUTO_INCREMENT,
+						  `userCaps` varchar(70) NOT NULL DEFAULT 'guest',
+						  `action` varchar(255) NOT NULL,
+						  `object_type` varchar(255) NOT NULL,
+						  `object_subtype` varchar(255) NOT NULL DEFAULT '',
+						  `object_name` varchar(255) NOT NULL,
+						  `object_id` int(11) NOT NULL DEFAULT '0',
+						  `user_id` int(11) NOT NULL DEFAULT '0',
+						  `histIP` varchar(55) NOT NULL DEFAULT '127.0.0.1',
+						  `histTime` int(11) NOT NULL DEFAULT '0',
+						  PRIMARY KEY (`histid`)
+					) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;";
+
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			dbDelta( $sql );
+
+		endif;
+		
+		update_option( 'history_timeline_db_version', '0.1' );
+	}
+
+	public static function uninstall() {
+		/** @var $wpdb wpdb */
+		global $wpdb;
+
+		$wpdb->query( "DROP TABLE IF EXISTS $wpdb->history_timeline" );
+
+		delete_option( 'history_timeline_db_version' );
 	}
 }
 
-register_activation_hook( HISTORY_TIMELINE_BASE, array( 'HT_Maintenance', 'activated' ) );
+register_activation_hook( HISTORY_TIMELINE_BASE, array( 'HT_Maintenance', 'activate' ) );
+register_uninstall_hook( HISTORY_TIMELINE_BASE, array( 'HT_Maintenance', 'uninstall' ) );
