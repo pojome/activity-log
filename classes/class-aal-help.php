6bd477c24927c33d6e069c756c549a21831820ec
Yakir Sitbon <kingyes1@gmail.com>
2014-04-17 10:29:41 +0000

    gettext support

--- classes/class-aal-help.php
+++ classes/class-aal-help.php
@@ -21,9 +21,9 @@ class AAL_Help {
                     'title' => __( 'Overview', 'aryo-aal' ),
                     'id' => 'aal-overview',
                     'content' => '
-                    <h3>Notifications</h3>
-                    <p>This screen lets you control what will happen once a user on your site does something you define. For instance, let us assume that you have created a user on your site
-                    for your content editor. Now, let\'s say that every time that user updates a post, you want to know about it. You can easily do it from this page.</p>',
+                    <h3>' . __( 'Notifications', 'aryo-aal' ) . '</h3>
+                    <p>' . __( 'This screen lets you control what will happen once a user on your site does something you define. For instance, let us assume that you have created a user on your site
+                    for your content editor. Now, let\'s say that every time that user updates a post, you want to know about it. You can easily do it from this page.', 'aryo-aal' ) . '</p>',
                 ) );
                 break;
         }
