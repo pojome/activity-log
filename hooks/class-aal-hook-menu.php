008f164d0c730266064ad4db50e0d7dbab18e3f5
Yakir Sitbon <kingyes1@gmail.com>
2014-04-25 17:58:08 +0000

    coding

--- hooks/class-aal-hook-menu.php
+++ hooks/class-aal-hook-menu.php
@@ -6,9 +6,9 @@ class AAL_Hook_Menu extends AAL_Hook_Base {
 	public function hooks_menu_updated( $nav_menu_selected_id ) {
 		if ( $menu_object = wp_get_nav_menu_object( $nav_menu_selected_id ) ) {
 			aal_insert_log( array(
-				'action'         => 'updated',
-				'object_type'    => 'Menu',
-				'object_name'    => $menu_object->name,
+				'action'      => 'updated',
+				'object_type' => 'Menu',
+				'object_name' => $menu_object->name,
 			) );
 		}
 	}
