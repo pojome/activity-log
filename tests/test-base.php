ce2b0ad2376fcd2e668847381a0cc2a1b024ec22
Yakir Sitbon <kingyes1@gmail.com>
2014-06-28 22:01:15 +0300

    WP Coding

--- tests/test-base.php
+++ tests/test-base.php
@@ -9,23 +9,27 @@ class AAL_Test_Base extends WP_UnitTestCase {
 	public function test_create_post() {
 		global $wpdb;
 		
-		$post = $this->factory->post->create_and_get( array(
-			'post_title' => 'aal-test-create-post',
-		) );
+		$post = $this->factory->post->create_and_get(
+			array(
+				'post_title' => 'aal-test-create-post',
+			)
+		);
 		
-		$row = $wpdb->get_row( $wpdb->prepare(
-			'SELECT * FROM %1$s
-				WHERE `action` = \'%2$s\'
-					AND `object_type` = \'%3$s\'
-					AND `object_subtype` = \'%4$s\'
-					AND `object_name` = \'%5$s\'
-			',
-			$wpdb->activity_log,
-			'updated',
-			'Post',
-			$post->post_type,
-			$post->post_title
-		) );
+		$row = $wpdb->get_row(
+			$wpdb->prepare(
+				'SELECT * FROM %1$s
+					WHERE `action` = \'%2$s\'
+						AND `object_type` = \'%3$s\'
+						AND `object_subtype` = \'%4$s\'
+						AND `object_name` = \'%5$s\'
+				',
+				$wpdb->activity_log,
+				'updated',
+				'Post',
+				$post->post_type,
+				$post->post_title
+			)
+		);
 		
 		$this->assertNotEmpty( $row );
 	}
@@ -33,25 +37,29 @@ class AAL_Test_Base extends WP_UnitTestCase {
 	public function test_delete_post() {
 		global $wpdb;
 		
-		$post = $this->factory->post->create_and_get( array(
-			'post_title' => 'aal-test-delete-post',
-		) );
+		$post = $this->factory->post->create_and_get(
+			array(
+				'post_title' => 'aal-test-delete-post',
+			)
+		);
 		
 		wp_delete_post( $post->ID );
 
-		$row = $wpdb->get_row( $wpdb->prepare(
-			'SELECT * FROM %1$s
-				WHERE `action` = \'%2$s\'
-					AND `object_type` = \'%3$s\'
-					AND `object_subtype` = \'%4$s\'
-					AND `object_name` = \'%5$s\'
-			',
-			$wpdb->activity_log,
-			'deleted',
-			'Post',
-			$post->post_type,
-			$post->post_title
-		) );
+		$row = $wpdb->get_row(
+			$wpdb->prepare(
+				'SELECT * FROM %1$s
+					WHERE `action` = \'%2$s\'
+						AND `object_type` = \'%3$s\'
+						AND `object_subtype` = \'%4$s\'
+						AND `object_name` = \'%5$s\'
+				',
+				$wpdb->activity_log,
+				'deleted',
+				'Post',
+				$post->post_type,
+				$post->post_title
+			)
+		);
 
 		$this->assertNotEmpty( $row );
 	}
