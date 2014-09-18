f2f93c2d9ce7550f7448cbc56485ba7f53caff65
Yakir Sitbon <kingyes1@gmail.com>
2014-09-18 19:28:01 +0300

    New! - hook when we create a new Menu

--- hooks/class-aal-hook-menu.php
+++ hooks/class-aal-hook-menu.php
@@ -5,9 +5,14 @@ class AAL_Hook_Menu extends AAL_Hook_Base {
 
 	public function hooks_menu_updated( $nav_menu_selected_id ) {
 		if ( $menu_object = wp_get_nav_menu_object( $nav_menu_selected_id ) ) {
+			if ( 'wp_create_nav_menu' === current_action() )
+				$action = 'created';
+			else
+				$action = 'updated';
+			
 			aal_insert_log(
 				array(
-					'action'      => 'updated',
+					'action'      => $action,
 					'object_type' => 'Menu',
 					'object_name' => $menu_object->name,
 				)
@@ -17,6 +22,7 @@ class AAL_Hook_Menu extends AAL_Hook_Base {
 	
 	public function __construct() {
 		add_action( 'wp_update_nav_menu', array( &$this, 'hooks_menu_updated' ) );
+		add_action( 'wp_create_nav_menu', array( &$this, 'hooks_menu_updated' ) );
 
 		parent::__construct();
 	}
