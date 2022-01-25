6ec4d6ba4a8cb774ade2a0a67c6bda7f553edc79
Yakir Sitbon <kingyes1@gmail.com>
2022-01-25 16:26:01 +0200

    Bump to 2.8.2

--- aryo-activity-log.php
+++ aryo-activity-log.php
@@ -5,7 +5,7 @@ Plugin URI: https://activitylog.io/?utm_source=wp-plugins&utm_campaign=plugin-ur
 Description: Get aware of any activities that are taking place on your dashboard! Imagine it like a black-box for your WordPress site. e.g. post was deleted, plugin was activated, user logged in or logged out - it's all these for you to see.
 Author: Activity Log Team
 Author URI: https://activitylog.io/?utm_source=wp-plugins&utm_campaign=author-uri&utm_medium=wp-dash
-Version: 2.8.1
+Version: 2.8.2
 Text Domain: aryo-activity-log
 License: GPLv2 or later
 
