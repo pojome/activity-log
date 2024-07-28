411be067b9db6c0b7b3dc130f300e396e2f41cb0
Yakir Sitbon <kingyes1@gmail.com>
2024-07-28 17:52:17 +0300

    Tweak: Monitoring enable/disable major core auto-updates [ED-15358] (#215)
    * Tweak: Monitoring enable/disable major core auto-updates [ED-15358]
    
    * minor text;

--- hooks/class-aal-hook-core.php
+++ hooks/class-aal-hook-core.php
@@ -7,10 +7,11 @@ class AAL_Hook_Core extends AAL_Hook_Base {
 		global $pagenow;
 
 		// Auto updated
-		if ( 'update-core.php' !== $pagenow )
+		if ( 'update-core.php' !== $pagenow ) {
 			$object_name = 'WordPress Auto Updated';
-		else
+		} else {
 			$object_name = 'WordPress Updated';
+		}
 
 		aal_insert_log(
 			array(
@@ -22,9 +23,26 @@ class AAL_Hook_Core extends AAL_Hook_Base {
 		);
 	}
 
+	public function hooks_auto_major_update_settings( $option, $value, $old_value ) {
+		if ( 'enabled' === $value ) {
+			$action = 'auto_update_enabled';
+		} else {
+			$action = 'auto_update_disabled';
+		}
+
+		aal_insert_log(
+			array(
+				'action' => $action,
+				'object_type' => 'Core',
+				'object_name' => 'All New Versions'
+			)
+		);
+	}
+
 	public function __construct() {
 		add_action( '_core_updated_successfully', array( &$this, 'core_updated_successfully' ) );
-		
+		add_action( 'update_site_option_auto_update_core_major', [ $this, 'hooks_auto_major_update_settings' ], 10, 3 );
+
 		parent::__construct();
 	}
 	
--- i18n/strings.php
+++ i18n/strings.php
@@ -34,6 +34,9 @@ __( 'activated', 'aryo-activity-log' );
 __( 'deactivated', 'aryo-activity-log' );
 __( 'installed', 'aryo-activity-log' );
 __( 'File Updated', 'aryo-activity-log' );
+__( 'deleted', 'aryo-activity-log' );
+__( 'auto_update_disabled', 'aryo-activity-log' );
+__( 'auto_update_enabled', 'aryo-activity-log' );
 
 // Themes
 __( 'Themes', 'aryo-activity-log' );
@@ -55,6 +58,7 @@ __( 'Taxonomies', 'aryo-activity-log' );
 __( 'Core', 'aryo-activity-log' );
 __( 'WordPress Updated', 'aryo-activity-log' );
 __( 'WordPress Auto Updated', 'aryo-activity-log' );
+__( 'All New Versions', 'aryo-activity-log' );
 
 // Emails
 __( 'Emails', 'aryo-activity-log' );
